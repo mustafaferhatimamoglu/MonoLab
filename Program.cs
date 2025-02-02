76805d32ba64f27729458454438881a06997919a
mustafaferhatimamoglu <65467821+mustafaferhatimamoglu@users.noreply.github.com>
2025-02-02 04:34:23 +0300

    hazır

--- Program.cs
+++ Program.cs
@@ -1,45 +1,102 @@
 ﻿using System;
 using System.IO;
 using System.Text.RegularExpressions;
-using System.Xml.Linq;
-using System.Linq;
 
 class Program
 {
+    static string directoryPath = "HAM";
+    static string directoryPath2 = "OLMUŞ";
     static void Main()
     {
-        string inputFile = "MONOLA01.pro";
-        string outputFile = "MONOLA01_düzeltilmiş.pro";
+        string[] proFiles = Directory.GetFiles(directoryPath, "*.pro");
 
+        foreach (var file in proFiles)
+        {
+            Donustur(file);
+        }
+    }
+
+    static void Donustur(string inputFile)
+    {
         try
         {
-            // Dosya içeriğini oku
-            string content = File.ReadAllText(inputFile);
+            #region OKU
+            //string[] lines = File.ReadAllLines(inputFile);
+            string[] lines = File.ReadAllLines(inputFile, System.Text.Encoding.Latin1);
+            int startLine = -1, endLine = -1;
 
-            // XML içeriğini bul ve işle
-            var piecesListMatch = Regex.Match(content, @"<PIECESLIST>(.*?)</PIECESLIST>", RegexOptions.Singleline);
-            if (piecesListMatch.Success)
+            for (int i = 0; i < lines.Length; i++)
             {
-                string piecesListXml = "<PIECESLIST>" + piecesListMatch.Groups[1].Value + "</PIECESLIST>";
-                var xmlDoc = XDocument.Parse(piecesListXml);
+                if (lines[i].Contains("<PIECESLIST>"))
+                    startLine = i + 1; // Satır numarası (0 tabanlı değil)
 
-                // <PIECE Code="X"> olan tüm kodları al
-                var pieces = xmlDoc.Descendants("PIECE")
-                    .Select(p => p.Attribute("Code")?.Value)
-                    .Where(code => !string.IsNullOrEmpty(code))
-                    .ToList();
-
-                // Her PIECE Code değerini dosya içinde ilgili yerlere yaz
-                for (int i = 0; i < pieces.Count; i++)
-                {
-                    string pattern = $@"(<PIECE Code="".*?"")";
-                    content = Regex.Replace(content, pattern, $@"$1 Value=""{pieces[i]}""", RegexOptions.Multiline);
-                }
+                if (lines[i].Contains("</PIECESLIST>"))
+                    endLine = i + 1;
             }
 
-            // Yeni dosyayı oluştur
-            File.WriteAllText(outputFile, content);
-            Console.WriteLine($"Düzenlenmiş dosya oluşturuldu: {outputFile}");
+            if (startLine != -1)
+            {
+                //Console.WriteLine($"<PIECESLIST> etiketi {startLine}. satırda.");
+            }
+            else
+            {
+                Console.WriteLine("<PIECESLIST> etiketi bulunamadı.");
+                throw new Exception("Format yanlış");
+            }
+            if (endLine != -1)
+            {
+                //Console.WriteLine($"</PIECESLIST> etiketi {endLine}. satırda.");
+            }
+            else
+            {
+                Console.WriteLine("</PIECESLIST> etiketi bulunamadı.");
+                throw new Exception("Format yanlış");
+            }
+            #endregion
+            #region tanımlı verileri yerlerine yaz
+            for (int i = startLine; i < endLine - 1; i++)
+            {
+                //Console.WriteLine(lines[i]);
+                string[] pieces = lines[i].Split("<PIECE Code=\"");
+                string[] pieces2 = pieces[1].Split("\" ");
+                var key = pieces2[0];
+                var value = pieces2[1];
+                //Console.WriteLine($"Key: {key}, Value: {value}");
+
+                for (int j = endLine; j < lines.Count(); j++)
+                {
+                    if (lines[j].Contains("\"" + key + "\""))
+                    {
+                        lines[j] = lines[j].Replace("\"" + key + "\"", value);
+                    }
+                }
+
+            }
+            #endregion
+            #region tanımsız verileri temizler
+            for (int j = endLine; j < lines.Count(); j++)
+            {
+                if (lines[j].Contains("\""))
+                {
+                    lines[j] = lines[j].Split("\"")[0];
+                }
+            }
+            #endregion
+            #region piecelist bölümünü sil
+
+            var newLines = new List<string>();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (i < (startLine - 1) || i >= endLine)
+                {
+                    newLines.Add(lines[i]);
+                }
+            }
+            lines = newLines.ToArray();
+
+            #endregion
+            File.WriteAllLines(directoryPath2 + "\\" + inputFile.Replace(directoryPath, ""), lines);
+            File.Delete(inputFile);
         }
         catch (Exception ex)
         {
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MonoLab
+
+1-)PieceList tablosu silinecek
+2-)PieceList'te olmayan pieceler veriden silinecek
+3-)PieceList'te karþýlýðý olan veriler için deðiþtir fonksiyonu kullanýlacak piecelist Key->Name
